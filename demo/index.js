fbfd77ac20b9166c98cd77dd621c9d87ec3f92fb
Flippie.Coetser <flippie.coetser@gmail.com>
2022-06-21 05:59:00 -0700

    Implement example use

--- demo/index.js
+++ demo/index.js
@@ -1,3 +1,9 @@
-import { Calculator } from 'calculator';
-console.log(`Import module: Calculator`)
-console.log(`Use module: 1 + 2 = ${Calculator.Add(1, 2)}`); 
+import { Dictionary } from 'dictionary';
+const dictionary = new Dictionary()
+
+const key = 'key'
+const value = 'value'
+
+dictionary.add(key,value)
+console.log(`Dictionary: add(${key},${value})`)
+console.log(`Dictionary: get(${key}) returns: ${dictionary.get(key)}`)
